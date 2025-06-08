8f817aa5e317836ad6a10406b5118079ce5e6bf1
Steven WdV <stevenwdv@gmail.com>
2025-06-08 20:06:39 +0200

    Allow direct insertion for unfiltered inserter check

--- scanners/unfiltered_inserters.lua
+++ scanners/unfiltered_inserters.lua
@@ -1,3 +1,10 @@
+local lualib_util = require "__core__.lualib.util"
+
+local is_allowed_dropoff_target = lualib_util.list_to_map{
+	"assembling-machine",
+	"furnace",
+}
+
 ---@param ctx ScanContext
 ---@return boolean @Found issue?
 local function scan_unfiltered_inserters(ctx)
@@ -25,24 +32,32 @@ local function scan_unfiltered_inserters(ctx)
 				},
 				type = { "inserter" },
 			}
-			local has_filtered = false
-			for _, inserter in pairs(inserters) do
-				if inserter.pickup_target == assembler then
-					if inserter.use_filters then
-						has_filtered = true
-						break
-					end
-				end
-			end
-			if has_filtered then
+			if #inserters > 1 then -- Quick exit
+				local has_filtered = false
+				-- Is one of the inserters filtered?
 				for _, inserter in pairs(inserters) do
 					if inserter.pickup_target == assembler then
-						if not inserter.use_filters then
-							unfiltered_inserters = unfiltered_inserters + 1
-							ctx:mark_entity(inserter, "unfiltered inserter", {
-								type = "entity",
-								name = inserter.name,
-							})
+						if inserter.use_filters then
+							has_filtered = true
+							break
+						end
+					end
+				end
+				if has_filtered then
+					-- Then mark the unfiltered ones
+					for _, inserter in pairs(inserters) do
+						if inserter.pickup_target == assembler then
+							if not inserter.use_filters then
+								local dropoff_target = inserter.drop_target
+								-- Allow direct insertion
+								if not (dropoff_target and is_allowed_dropoff_target[dropoff_target.type]) then
+									unfiltered_inserters = unfiltered_inserters + 1
+									ctx:mark_entity(inserter, "unfiltered inserter", {
+										type = "entity",
+										name = inserter.name,
+									})
+								end
+							end
 						end
 					end
 				end
